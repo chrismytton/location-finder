5236874c80998d75b0b18f27e8e345093608ca3c
Chris Mytton <chrismytton@gmail.com>
2020-06-07 22:05:08 +0100

    Better display of values

--- src/components/LocationFinder.vue
+++ src/components/LocationFinder.vue
@@ -3,7 +3,7 @@
     <div v-if="locating">
       Locating&hellip;
     </div>
-    <div v-if="position">
+    <div v-else-if="position">
       <table>
         <tr>
           <td>Latitude</td>
@@ -21,19 +21,28 @@
         </tr>
         <tr>
           <td>Heading</td>
-          <td>{{ heading }}</td>
+          <td>
+            <span v-if="heading">{{ heading }}</span>
+          </td>
         </tr>
         <tr>
           <td>Speed</td>
-          <td>{{ speed }}</td>
+          <td>
+            <span v-if="speed">{{ speed }} metres per second</span>
+          </td>
         </tr>
         <tr>
           <td>Accuracy</td>
-          <td>{{ accuracy }}</td>
+          <td>
+            <span v-if="accuracy">+- {{ accuracy }} metres</span>
+          </td>
         </tr>
         <tr>
           <td>Altitude Accuracy</td>
-          <td>{{ altitudeAccuracy }}</td>
+
+          <td>
+            <span v-if="altitudeAccuracy">+- {{ altitudeAccuracy }} metres</span>
+          </td>
         </tr>
       </table>
       <p>
@@ -62,13 +71,23 @@ export default {
     return {
       locating: false,
       position: null,
+      coords: {},
       error: {},
       watchID: null,
     };
   },
+  created() {
+    if (localStorage.getItem('gpsSuccess')) {
+      this.startTracking()
+    }
+  },
   methods: {
+
     onPositionUpdate(position) {
+      localStorage.setItem('gpsSuccess', true)
+
       this.position = position;
+      this.coords = position.coords
       this.locating = false;
     },
     onPositionError(err) {
@@ -77,35 +96,57 @@ export default {
     },
     startTracking() {
       this.locating = true
-      navigator.geolocation.clearWatch(this.watchID)
+      if (this.watchID) {
+        navigator.geolocation.clearWatch(this.watchID)
+      }
       this.watchID = navigator.geolocation.watchPosition(
         this.onPositionUpdate,
-        this.onPositionError,
-        { enableHighAccuracy: true }
+        this.onPositionError
       );
     }
   },
   computed: {
     latitude() {
-      return this.position.coords.latitude.toFixed(4);
+      if (!this.coords.latitude) {
+        return
+      }
+      return this.coords.latitude.toFixed(4);
     },
     longitude() {
-      return this.position.coords.longitude.toFixed(4);
+      if (!this.coords.longitude) {
+        return
+      }
+      return this.coords.longitude.toFixed(4);
     },
     altitude() {
-      return this.position.coords.altitude.toFixed(4);
+      if (!this.coords.altitude) {
+        return
+      }
+      return this.coords.altitude.toFixed(2);
     },
     heading() {
-      return this.position.coords.heading.toFixed(4);
+      if (!this.coords.heading) {
+        return
+      }
+      return this.coords.heading.toFixed(0);
     },
     speed() {
-      return this.position.coords.speed.toFixed(4);
+      if (!this.coords.speed) {
+        return
+      }
+      return this.position.coords.speed.toFixed(1);
     },
     accuracy() {
-      return this.position.coords.accuracy.toFixed(4);
+      if (!this.coords.accuracy) {
+        return
+      }
+      return this.position.coords.accuracy
     },
     altitudeAccuracy() {
-      return this.position.coords.altitudeAccuracy.toFixed(4);
+      if (!this.coords.altitudeAccuracy) {
+        return
+      }
+      return this.position.coords.altitudeAccuracy;
     },
     errorMessage() {
       if (!this.error.code || !this.error.message) {
