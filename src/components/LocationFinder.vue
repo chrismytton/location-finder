d2be29f5391c376d765f496963a377e6b3f86d9b
Chris Mytton <mail@chrismytton.com>
2023-01-29 00:02:58 +0000

    Smooth the boxes

--- src/components/LocationFinder.vue
+++ src/components/LocationFinder.vue
@@ -207,29 +207,29 @@ export default {
 <template>
   <div class="m-5">
     <div v-if="position" class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 min-w-0">
-      <div @click="copy(latitude)" class="flex flex-col gap-3 text-center p-5 border justify-center">
+      <div @click="copy(latitude)" class="flex flex-col gap-3 rounded-xl text-center p-5 border justify-center">
         <span class="text-xl">Latitude</span>
         <span class="text-3xl" v-bind:title="locationAccuracy">{{ latitude }}</span>
       </div>
-      <div @click="copy(longitude)" v-if="longitude" class="flex flex-col gap-3 text-center p-5 border justify-center">
+      <div @click="copy(longitude)" v-if="longitude" class="flex flex-col gap-3 rounded-xl text-center p-5 border justify-center">
         <span class="text-xl">Longitude</span>
         <span class="text-3xl" v-bind:title="locationAccuracy">{{ longitude }}</span>
       </div>
-      <div @click="copy(altitude)" v-if="altitude" class="flex flex-col text-center p-5 border justify-center"><span class="text-xl mb-3">Altitude</span> <span class="text-3xl mb-3">{{ altitude }} meters</span><span>+-{{ altitudeAccuracy }} meters</span></div>
-      <div @click="copy(heading)" class="flex flex-col text-center p-5 border justify-center"><span class="text-xl mb-3">Heading</span> <span class="text-3xl" v-html="heading"></span></div>
-      <div @click="copy(mph)" v-if="mph" class="flex flex-col text-center p-5 border justify-center"><span class="text-xl mb-3">Miles per hour</span> <span class="text-3xl">{{ mph }} mph</span></div>
-      <div @click="copy(kph)" v-if="kph" class="flex flex-col text-center p-5 border justify-center"><span class="text-xl mb-3">Kilometers per hour</span> <span class="text-3xl">{{ kph }} km/h</span></div>
-      <div @click="copy(speed)" v-if="speed" class="flex flex-col text-center p-5 border justify-center"><span class="text-xl mb-3">Meters per second</span> <span class="text-3xl">{{ speed }} m/s</span></div>
-      <div @click="copy(JSON.stringify(geoJSON, null, 2))" v-if="geoJSON" class="flex flex-col p-5 border justify-center"><span class="text-xl mb-3 text-center">GeoJSON</span> <pre class="overflow-x-scroll shadow-inner p-3">{{ geoJSON }}</pre></div>
-      <div @click="copy(wktWithAltitude)" v-if="altitude" class="flex flex-col text-center p-5 border justify-center"><span class="text-xl mb-3">WKT</span> <pre class="overflow-x-scroll shadow-inner p-3">{{ wktWithAltitude }}</pre></div>
-      <div @click="copy(wkt)" v-else class="flex flex-col text-center p-5 border justify-center"><span class="text-xl mb-3">WKT</span> <pre class="overflow-x-scroll shadow-inner p-3">{{ wkt }}</pre></div>
-      <div class="flex flex-col text-center p-5 border justify-center">
+      <div @click="copy(altitude)" v-if="altitude" class="flex flex-col text-center rounded-xl p-5 border justify-center"><span class="text-xl mb-3">Altitude</span> <span class="text-3xl mb-3">{{ altitude }} meters</span><span>+-{{ altitudeAccuracy }} meters</span></div>
+      <div @click="copy(heading)" class="flex flex-col text-center rounded-xl p-5 border justify-center"><span class="text-xl mb-3">Heading</span> <span class="text-3xl" v-html="heading"></span></div>
+      <div @click="copy(mph)" v-if="mph" class="flex flex-col text-center rounded-xl p-5 border justify-center"><span class="text-xl mb-3">Miles per hour</span> <span class="text-3xl">{{ mph }} mph</span></div>
+      <div @click="copy(kph)" v-if="kph" class="flex flex-col text-center rounded-xl p-5 border justify-center"><span class="text-xl mb-3">Kilometers per hour</span> <span class="text-3xl">{{ kph }} km/h</span></div>
+      <div @click="copy(speed)" v-if="speed" class="flex flex-col text-center rounded-xl p-5 border justify-center"><span class="text-xl mb-3">Meters per second</span> <span class="text-3xl">{{ speed }} m/s</span></div>
+      <div @click="copy(JSON.stringify(geoJSON, null, 2))" v-if="geoJSON" class="flex flex-col p-5 rounded-xl border justify-center"><span class="text-xl mb-3 text-center">GeoJSON</span> <pre class="overflow-x-scroll shadow-inner p-3">{{ geoJSON }}</pre></div>
+      <div @click="copy(wktWithAltitude)" v-if="altitude" class="flex flex-col text-center rounded-xl p-5 border justify-center"><span class="text-xl mb-3">WKT</span> <pre class="overflow-x-scroll shadow-inner p-3">{{ wktWithAltitude }}</pre></div>
+      <div @click="copy(wkt)" v-else class="flex flex-col text-center rounded-xl p-5 border justify-center"><span class="text-xl mb-3">WKT</span> <pre class="overflow-x-scroll shadow-inner p-3">{{ wkt }}</pre></div>
+      <div class="flex flex-col text-center rounded-xl p-5 border justify-center">
         <span class="text-xl mb-3">Maps</span>
         <a class="p-4 bg-blue-500 text-zinc-50 my-3 text-xl font-semibold" v-bind:href="`https://www.google.co.uk/maps/@${latitude},${longitude},17z`" target="_blank">Google Maps</a>
         <a class="p-4 bg-blue-500 text-zinc-50 my-3 text-xl font-semibold" v-bind:href="`https://www.openstreetmap.org/#map=17/${latitude}/${longitude}`" target="_blank">OpenStreetMap</a>
         <a class="p-4 bg-blue-500 text-zinc-50 my-3 text-xl font-semibold" v-bind:href="`https://www.bing.com/maps?lvl=17&cp=${latitude}~${longitude}`" target="_blank">Bing</a>
       </div>
-      <div @click="copy(timestamp)" v-if="timestamp" class="flex flex-col text-center p-5 border justify-center"><span class="text-xl mb-3">Last updated</span> <span class="text-3xl" v-bind:title="timestamp">{{ relativeTimestamp }} seconds ago</span></div>
+      <div @click="copy(timestamp)" v-if="timestamp" class="flex flex-col text-center rounded-xl p-5 border justify-center"><span class="text-xl mb-3">Last updated</span> <span class="text-3xl" v-bind:title="timestamp">{{ relativeTimestamp }} seconds ago</span></div>
     </div>
     <div v-else-if="locating" class="grid justify-center content-center h-screen text-3xl font-bold">
       Locating&hellip;
